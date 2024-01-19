8060d23f227b047f10325ff05a9e5ca0f196832b
emendoza96 <emilianomendoza294@gmail.com>
2024-01-19 14:39:24 -0300

    Add swagger documentation to ConstructionController endpoints

--- src/main/java/com/microservice/user/controller/ConstructionController.java
+++ src/main/java/com/microservice/user/controller/ConstructionController.java
@@ -7,6 +7,11 @@ import com.microservice.user.domain.Construction;
 import com.microservice.user.domain.ConstructionType;
 import com.microservice.user.domain.Customer;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,26 +24,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/construction")
+@Api(value = "ConstructionRest")
 public class ConstructionController {
 
-    @PostMapping
-    public Construction saveConstruction(@RequestBody Construction construction) {
-
-        System.out.println(construction);
-
-        return construction;
-    }
-
-    @PutMapping("/edit/{id}")
-    public Construction editConstruction(@PathVariable Integer id, @RequestBody Construction construction) {
-
-        construction.setId(id);
-        System.out.println(construction);
-
-        return construction;
-    }
 
     @GetMapping
+    @ApiOperation(value = "Get a construction by parameters")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Get request successfully completed"),
+        @ApiResponse(code = 401, message = "Not authorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "Construction not found with the parameters provided")
+    })
     public Construction getConstruction(
         @RequestParam Integer id,
         @RequestParam(required = false) String customerName,
@@ -63,7 +60,47 @@ public class ConstructionController {
         return construction;
     }
 
+
+    @PostMapping
+    @ApiOperation(value = "Save a new construction")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Construction successfully saved"),
+        @ApiResponse(code = 401, message = "Not authorized"),
+        @ApiResponse(code = 403, message = "Forbidden")
+    })
+    public Construction saveConstruction(@RequestBody Construction construction) {
+
+        System.out.println(construction);
+
+        return construction;
+    }
+
+
+    @PutMapping("/edit/{id}")
+    @ApiOperation(value = "Edit a construction")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Construction successfully edited"),
+        @ApiResponse(code = 401, message = "Not authorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "Construction not found")
+    })
+    public Construction editConstruction(@PathVariable Integer id, @RequestBody Construction construction) {
+
+        construction.setId(id);
+        System.out.println(construction);
+
+        return construction;
+    }
+
+
     @DeleteMapping("/delete/{id}")
+    @ApiOperation(value = "Edit a construction")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Construction successfully deleted"),
+        @ApiResponse(code = 401, message = "Not authorized"),
+        @ApiResponse(code = 403, message = "Forbidden"),
+        @ApiResponse(code = 404, message = "Construction not found")
+    })
     public Boolean deleteConstruction(@PathVariable Integer id) {
 
         return true;
