3f4be9555c4e40d20b09745daaca1c94b90dc7f9
emendoza96 <emilianomendoza294@gmail.com>
2025-03-23 18:21:44 -0300

    Add redis config

--- src/main/java/com/microservice/user/config/RedisConfig.java
+++ src/main/java/com/microservice/user/config/RedisConfig.java
@@ -1,10 +1,23 @@
 package com.microservice.user.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "com.microservice.user.dao.redis")
 public class RedisConfig {
-}
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
+    }
+}
